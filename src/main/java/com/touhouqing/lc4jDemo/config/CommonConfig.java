4a801bfe710bee8c041c715e7f8845c872a5be3a
touhouqing <qinghaoyang@foxmail.com>
2025-08-04 16:17:13 +0800

    init pdf-parser and splitter

--- src/main/java/com/touhouqing/lc4jDemo/config/CommonConfig.java
+++ src/main/java/com/touhouqing/lc4jDemo/config/CommonConfig.java
@@ -2,7 +2,11 @@ package com.touhouqing.lc4jDemo.config;
 
 import com.touhouqing.lc4jDemo.repository.RedisChatMemoryStore;
 import dev.langchain4j.data.document.Document;
+import dev.langchain4j.data.document.DocumentSplitter;
 import dev.langchain4j.data.document.loader.ClassPathDocumentLoader;
+import dev.langchain4j.data.document.loader.FileSystemDocumentLoader;
+import dev.langchain4j.data.document.parser.apache.pdfbox.ApachePdfBoxDocumentParser;
+import dev.langchain4j.data.document.splitter.DocumentSplitters;
 import dev.langchain4j.memory.ChatMemory;
 import dev.langchain4j.memory.chat.ChatMemoryProvider;
 import dev.langchain4j.memory.chat.MessageWindowChatMemory;
@@ -56,12 +60,19 @@ public class CommonConfig {
     @Bean
     public EmbeddingStore store(){
         //1. 加载文档进内存
-        List<Document> documents = ClassPathDocumentLoader.loadDocuments("content");
+//        List<Document> documents = ClassPathDocumentLoader.loadDocuments("content");
+        List<Document> documents = ClassPathDocumentLoader.loadDocuments("content",new ApachePdfBoxDocumentParser());
+        //List<Document> documents = FileSystemDocumentLoader.loadDocuments("src/main/resources/content");
         //2. 构建向量数据库操作对象
         InMemoryEmbeddingStore store = new InMemoryEmbeddingStore();
+
+        //构建文本分割器对象
+        DocumentSplitter ds = DocumentSplitters.recursive(500, 100);
+
         //3. 完成文本数据切割，向量化，存储
         EmbeddingStoreIngestor ingestor = EmbeddingStoreIngestor.builder()
                 .embeddingStore(store)
+                .documentSplitter(ds)
                 .build();
         ingestor.ingest(documents);
         return store;
